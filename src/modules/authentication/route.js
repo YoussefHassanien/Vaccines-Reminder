9daf41f88763f1bced83a42b1ef5753368de2f20
Youssef Hassanien <Youssefhassanein17@gmail.com>
2025-06-08 01:00:58 +0300

    Added login and signup validations

--- src/modules/authentication/route.js
+++ src/modules/authentication/route.js
@@ -8,22 +8,21 @@ import {
   updatePassword,
 } from "./controller.js";
 import {
+  signupValidator,
+  loginValidator,
   forgotPasswordValidator,
   resetPasswordValidator,
   verifyOTPValidator,
   updatePasswordValidator,
 } from "./validation.js";
-import {
-  isAuthenticated,
-  isAuthorized,
-} from "../../middlewares/authentication.js";
+import { isAuthenticated } from "../../middlewares/authentication.js";
 
 const authRouter = Router();
 
 // Authentication routes
-authRouter.post("/signup", signup);
-authRouter.post("/login", login);
+authRouter.post("/signup", signupValidator, signup);
 
+authRouter.post("/login", loginValidator, login);
 
 // Update Password route
 authRouter.put(
@@ -33,11 +32,9 @@ authRouter.put(
   updatePassword
 );
 
-
 // Forgot Password routes
 authRouter.post("/forgot-password", forgotPasswordValidator, forgotPassword);
 authRouter.post("/verify-otp", verifyOTPValidator, verifyForgotPasswordOTP);
 authRouter.post("/reset-password", resetPasswordValidator, resetPassword);
-
 
 export default authRouter;
